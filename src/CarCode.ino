6db68e8d62b3471c9f08fe08c233626af28f2459
Filip I <filip.saver15@gmail.com>
2017-05-22 17:33:57 +0200

    finally made it work properly
    Sends the sensor values in order {int, int, int, int, int}.
    Expects a string like "AsaaA", where 's' is the speed and 'aa' is the angle. Example: "A130A" - speed 1, angle 30.

--- src/CarCode.ino
+++ src/CarCode.ino
@@ -13,8 +13,8 @@
 #include <SharpIR.h>
 
 //definitions of the sensors
-#define SRF_ADDRESS1         0x70
-#define SRF_ADDRESS2         0x71        // Address of the SRF08
+#define SRF_ADDRESS1         0x71
+#define SRF_ADDRESS2         0x70        // Address of the SRF08
 #define IR_REAR              3
 #define IR_REAR_RIGHT        5
 #define IR_FRONT_RIGHT       4
@@ -23,7 +23,7 @@
 #define RANGEBYTE            0x02        // Byte for start of ranging data
 
 //lights
-#define NUM_LEDS             16
+#define NUM_LEDS             8
 #define led_lights           11
 CRGB leds[NUM_LEDS];
 
@@ -54,13 +54,17 @@ char userInput;
 int steerCarOutput         = 9 ;
 int SpeedCarOutput         = 10;
 
+int inputValues[] = { -1, -1, -1};
+int inputSpeed = 0;
+int inputSteer = 70;
+
 //Movementdeclaration
 Servo EscServo;
 Servo SteerServo;
 Odometer encoder;
 
 /**
-  Method created in order to get the range from the infrared.
+ * Method created in order to get the range from the infrared.
 */
 unsigned char getInfraredRange(int sensor) {
   unsigned char range = 0;
@@ -78,7 +82,7 @@ unsigned char getInfraredRange(int sensor) {
 }
 
 /**
-  Method created in order to get the range from the ultrasound.
+ * Method created in order to get the range from the ultrasound.
 */
 unsigned char getUltrasonicRange(int sensor) {                                  // This function gets a ranging from the SRF08
   unsigned char range = 0;
@@ -94,7 +98,7 @@ unsigned char getUltrasonicRange(int sensor) {                                  
   Wire.write(0x51);                                // Send 0x51 to start a ranging
   Wire.endTransmission();
 
-  delay(100);                                      // Wait for ranging to be complete
+  delay(80);                                      // Wait for ranging to be complete
 
   // start communicating with SRFmodule
   if (sensor == 1) {
@@ -121,30 +125,17 @@ unsigned char getUltrasonicRange(int sensor) {                                  
     range = range << 8;
     range |= Wire.read();
 
-    if (range > 50) {
+    if (range > 45) {
       range = 0;
     }
 
     return (range);
   }
 
-  if (range > 40) {
+  if (range > 45) {
     range = 0;
   }
   return (0);                                  // Returns Range
-}
-
-
-/*
-    Method created in order to read a byte from serial port, current speed and angle.
-    Iy converts them into floats.
-*/
-void byteDecode(char byteIn, float *speedIn, float * angle) {
-  float tempAng = *angle;
-  *angle = ((byteIn & 31) - 15) * 4;
-  //Serial2.println("qwdwe  "+String(((float)((byteIn & 224) >> 5)-3)));
-  //Serial2.println("abba  "+String(((float)((byteIn & 224) >> 5)-3)/2));
-  *speedIn = ((float)((byteIn & 224) >> 5) - 3) / 2;
 }
 
 
@@ -153,7 +144,9 @@ void byteDecode(char byteIn, float *speedIn, float * angle) {
 */
 void readSensors() {
   usFront = getUltrasonicRange(1);          // Calls a function to get range
+  delay(1);
   usFrontRight = getUltrasonicRange(2);
+  delay(1);
   irFrontRight = getInfraredRange(IR_FRONT_RIGHT);
   irRear =  getInfraredRange(IR_REAR);
   irRearRight = getInfraredRange(IR_REAR_RIGHT);
@@ -161,33 +154,37 @@ void readSensors() {
   unsigned char startTransmissionVal = 255;
   unsigned char endTransmissionVal = 254;
 
-  //Serial.write(startTransmissionVal);   //for sending the bytes
-  Serial.print(startTransmissionVal);     //debugging
-  Serial.print(" ");            //debbuging
+  //  Serial.write(startTransmissionVal);   //for sending the bytes
+  //  Serial.print(startTransmissionVal);     //debugging
+  //  Serial.print(" ");            //debbuging
 
-  //Serial.write(usFront);
+  Serial.print("{");
+
+  //  Serial.write(usFront);
   Serial.print(usFront);
   Serial.print(" ");
 
-  //Serial.write(usFrontRight);
+  //  Serial.write(usFrontRight);
   Serial.print(usFrontRight);
   Serial.print(" ");
 
-  //Serial.write(irFrontRight);
+  //  Serial.write(irFrontRight);
   Serial.print(irFrontRight);
   Serial.print(" ");
 
-  //Serial.write(irRear);
+  //  Serial.write(irRear);
   Serial.print(irRear);
   Serial.print(" ");
 
-  //Serial.write(irRearRight);
+  //  Serial.write(irRearRight);
   Serial.print(irRearRight);
-  Serial.print(" ");
+  //  Serial.print(" ");
 
-  //Serial.write(endTransmissionVal);
-  Serial.print(endTransmissionVal);
-  Serial.print("\n");
+  Serial.print("}");
+
+  //  Serial.write(endTransmissionVal);
+  //  Serial.print(endTransmissionVal);
+  //  Serial.print("\n");
 }
 
 
@@ -217,6 +214,12 @@ void setup() {
   delay(100);                                     // Waits to make sure everything is powered up
 
   FastLED.addLeds<WS2811, led_lights, RGB>(leds, NUM_LEDS);
+  delay(100);
+
+  Serial.print("A070A");
+  inputSpeed = 0;
+  inputSteer = 70;
+  delay(100);
 }
 
 void loop() {
@@ -253,12 +256,16 @@ void loop() {
 
     int i = pulseIn(5, HIGH); //Get speed
     int j = pulseIn(6, HIGH); //Get steering
+    Serial.print(" ");
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.print(j);
     j = j - 275;
 
     /*
-       This delay makes sure that is doesn't receive too much information the servo
+     * This delay makes sure that is doesn't receive too much information the servo
     */
-    delay(10);
+    delay(5);
 
     if (i > FULL) {
       i = FULL;
@@ -273,39 +280,54 @@ void loop() {
   }
   else
   {
+
+    SteerServo.write(inputSteer);
+
     if (controller) {
       speedValue = NEUTRAL;
 
       speedValueRev = NEUTRALREVERSE;
       steerValue = straight;
       controller = false;
-
-      SteerServo.write(straight);
-      EscServo.writeMicroseconds(NEUTRAL);
-
     }
-    unsigned char readSignal = Serial.read();
-    if (readSignal == 31) {
-      readSignal = Serial.read();
-      unsigned char angleSignal = readSignal;
-      angleSignal &= ~(1 << 7);
-      angleSignal &= ~(1 << 6);
-      angleSignal &= ~(1 << 5);
 
-      unsigned char speedSignal = readSignal;
-      speedSignal = speedSignal >> 5;
+    //How the received string should look:
+    //A22A or A122A, A199A (A start and end char, first digit is speed, others angle)
 
-      //int steering = words[1].toInt();
-      SteerServo.write(angleSignal);
+    if (Serial.read() == 'A') {
+      inputValues[0] = Serial.read() - '0';
+      inputValues[1] = Serial.read() - '0';
+      inputValues[2] = Serial.read() - '0';
+    }
+    if (inputValues[2] < 10 && inputValues[2] >= 0) {
+      inputValues[1] *= 10;
+      inputValues[1] += inputValues[2];
+    }
+    
+    /*
+       inputValues[0] - speed integer
+       inputValues[1] - angle integer
+    */
+    if ((inputValues[0] >= 0 && inputValues[0] < 10) || (inputValues[1] >= 0 && inputValues[1] < 99)) {
+      inputSpeed = inputValues[0];
+      inputSteer = inputValues[1];
 
-      if (speedSignal == 0) {
+      SteerServo.write(inputSteer);
+
+      if ( inputSpeed == 9 || inputSpeed == 0) {
         EscServo.writeMicroseconds(1400);
-      }
-      if (speedSignal > 0 && speedSignal < 6) {
-        EscServo.writeMicroseconds((speedSignal * 50) + 1400);
-      }
-      if (speedSignal > 5) {
+        //Serial.println("Stop!");
+
+      } else if (inputSpeed > 0 && inputSpeed < 6) {
+        //inputSpeed *=50;
+        int tmpInt = (inputSpeed * 50) + 1400;
+        EscServo.writeMicroseconds( tmpInt);
+        //Serial.println(tmpInt);
+
+      } else if (inputSpeed == 5 || inputSpeed == 6) {
         EscServo.writeMicroseconds(1150);
+        //Serial.println("reverse");
+
       }
     }
   }
@@ -317,7 +339,5 @@ void loop() {
     speedValue = BREAK;
   }
 
-  delay(1);
- // FastLED.stop();
+  //delay(100);
 }
-
